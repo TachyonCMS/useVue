48396e8b4d155a6ef3550789b0072bb6a406e8a8
Brian Winkers <bwinkers@gmail.com>
2022-08-11 00:57:39 -0700

    Initial site

---
+++ webapp/src/layouts/MainLayout.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+
+        <q-toolbar-title>
+          useVue.org - Tools to speed Vue adoption
+        </q-toolbar-title>
+
+        <div class="text-caption">Est. Aug 2022</div>
+      </q-toolbar>
+    </q-header>
+
+
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+  },
+
+  setup () {
+
+
+    return {
+
+    }
+  }
+})
+</script>
---
+++ webapp/src/pages/IndexPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card flat>
+      <q-card-section>
+         <div class="text-h2 row col-12 text-center justify-center">Welcome to useVue.org</div>
+      </q-card-section>
+
+    <q-card-section>
+       <div class="text-body1 row col-12 text-center justofy-center">Our mission is to provide resources and information that make it easier for businesses to adopt Vue.</div>
+
+    </q-card-section>
+    </q-card>
+    </q-page>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
